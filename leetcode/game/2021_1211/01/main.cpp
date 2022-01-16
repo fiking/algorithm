9c7dc8c5b9510a8efa6465b295cff0a38faa824a
fiking <1129277432@qq.com>
2022-01-16 23:43:52 +0800

    [leetcode] finish Biweekly Contest67 & Weekly Contest270 & Weekly Contest276

---
+++ leetcode/game/2021_1211/01/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        vector<int> stack;
+        for ()
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
